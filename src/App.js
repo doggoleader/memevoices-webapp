220a460d2ddb478b07adf6b0c1a10a4696a0c9c3
Doggo <doggoleader.06@gmail.com>
2025-02-22 20:55:12 +0300

    website update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import {useEffect} from "react";
-import Header from "./components/header/Header";
 import {useTelegram} from "./components/hooks/useTelegram";
 import { Route, Routes } from "react-router-dom";
 import Form from "./components/form/Form";
 import ProductList from "./components/ProductList/ProductList";
-import CloseButton from "./components/buttons/CloseButton/CloseButton";
 import Button from "./components/buttons/button/Button";
+import Header from "./components/header/Header";
 
 function App() {
 
@@ -19,6 +18,7 @@ function App() {
 
   return (
     <div className="App">
+        <Header />
         <Routes>
             <Route index element={<ProductList />} />
             <Route path={"form"} element={<Form />} />
