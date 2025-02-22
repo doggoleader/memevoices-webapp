43f33fdd6b8dcb51c8a7c6418c738879c3f06499
Doggo <doggoleader.06@gmail.com>
2025-02-22 20:48:20 +0300

    website update

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import {useTelegram} from "./components/hooks/useTelegram";
 import { Route, Routes } from "react-router-dom";
 import Form from "./components/form/Form";
 import ProductList from "./components/ProductList/ProductList";
+import CloseButton from "./components/buttons/CloseButton/CloseButton";
+import Button from "./components/buttons/button/Button";
 
 function App() {
 
-    const { telegram, onToggleButton } = useTelegram();
+    const { telegram, onClose } = useTelegram();
 
     useEffect(() => {
         telegram.ready();
     }, []);
 
+
   return (
     <div className="App">
-        <Header/>
         <Routes>
             <Route index element={<ProductList />} />
             <Route path={"form"} element={<Form />} />
         </Routes>
+        <Button onClick={onClose}>Закрыть</Button>
     </div>
   );
 }
